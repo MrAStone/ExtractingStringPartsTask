705bfd5cbcd62bc0af02f4b32b02c219ac764ed0
Al Stone <astone@heckgrammar.co.uk>
2024-06-28 10:47:34 +0100

    Add project files.

---
+++ ExtractingStringPartsTask/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace ExtractingStringPartsTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string country = "United States of America";
+            string state = "California";
+            string city = "San Fransicso";
+            string landmark = "Alcatraz Island";
+            // output the length of state
+
+            // Concatenate the contents of city and landmark * don't forget to add a space
+
+            // output the first character of county
+
+            // output a concatenation of the first and last characters of city
+
+            // output the substring of landmark starting at c and outputting to the end
+
+            // output the substring of country starting at the first S and ending at the first A
+
+            // output the position of f in California
+
+            //************CHALLENGE****************//
+            // Output how many vowels are in each of the strings in the format
+            // California has n vowels
+
+
+            
+
+            // Show your evidence of the program running in the Readme file
+        }
+    }
+}
